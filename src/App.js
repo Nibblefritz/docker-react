39746b41c516c494a8bf76e7a3ff57109aa3ca9f
Nibblefritz <kyle@fifieldonline.com>
2019-07-23 20:32:55 -0600

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hey you. How's it hanging?
+          This is my feature
         </p>
         <a
           className="App-link"
